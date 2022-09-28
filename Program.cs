e046c381cb3ef2250d2b0600aeb95b45426f2bd6
Oskar <106465473+OsqaR0@users.noreply.github.com>
2022-09-28 17:27:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Pętle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While | For | Do While
+            int a = 0;
+            while(a <= 5)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine("Tekst");
+                a++;
+            }
+
+            for(int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            do
+            {
+                Console.WriteLine("Text");
+            }
+            while (false);
+
+
+            Console.ReadKey();   
+        }
+    }
+}
